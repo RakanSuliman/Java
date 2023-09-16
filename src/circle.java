abdfd8d93ac7e3313b4882dcb4c1dd78784b2a50
RakanSuliman <rakanbinsuliman@gmail.com>
2023-09-16 14:21:01 +0300

    0.1.1

--- src/circle.java
+++ src/circle.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 public class circle {
     public static void main(String[] args) {
-        double PI = 3.14;
+        double PI = 3.14159;
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the radius value");
         double radius = input.nextDouble();
         if (radius <= 0) {
             System.out.println("Error, only positive numbers allowed");
         } else if (radius > 0) {
-            double circle = 2 * (PI * radius);
+            double perimeter  = 2 * (PI * radius);
             double area = (PI * radius * radius);
-            System.out.println("The area of a circle is: " + area + "\n" + "The perimeter of circle is: " + circle);
+            System.out.println("The area of a circle is: " + area + "\n" + "The perimeter of circle is: " + perimeter );
         }
     }
 }

21ae1c07c5a9e4f71c25f756608cc9d2702afae5
RakanSuliman <rakanbinsuliman@gmail.com>
2023-10-08 22:20:06 +0300

    0.1.2

--- src/NumberOfDaysInAMonth.java
+++ src/NumberOfDaysInAMonth.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class NODM {
+public class NumberOfDaysInAMonth {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a month: ");
--- src/rootsOfQuadratic.java
+++ src/rootsOfQuadratic.java
@@ -1,40 +1,34 @@
-import java.sql.SQLSyntaxErrorException;
 import java.util.Scanner;
-public class rootsOfQuadratic {
+
+public class rootsOfQuadratic  {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Input a: ");
+        System.out.print("Input a: ");
         double a = input.nextDouble();
-        System.out.println("Input b: ");
+        System.out.print("Input b: ");
         double b = input.nextDouble();
-        System.out.println("Input c: ");
+        System.out.print("Input c: ");
         double c = input.nextDouble();
 
+        double discriminant = b * b - 4 * a * c;
+        double equalZ = -b / (2 * a);
 
-        double discriminant = b*b-4*a*c;
-        double equalZ = -b/(2*a);
-        if (discriminant>0) {
-           double root1 = -b+Math.sqrt(discriminant)/(2*a);
-            double root2 = -b-Math.sqrt(discriminant)/(2*a);
-            System.out.println("1= " + root1);
-            System.out.println("1= " + root2);
-        } else if (discriminant==0) {
-
-           System.out.println("2= " + equalZ);
-
-        } else if (discriminant<0) {
+        if (discriminant > 0) {
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            System.out.println("Root 1 = " + root1);
+            System.out.println("Root 2 = " + root2);
+        } else if (discriminant == 0) {
+            System.out.println("Equal Roots = " + equalZ);
+        } else if (discriminant < 0) {
             double real = equalZ;
-            double imagin1 = Math.sqrt(-discriminant)/(2*a);
-            double imagin2 = Math.sqrt(+discriminant)/(2*a);
+            double imaginary1 = Math.sqrt(-discriminant) / (2 * a);
+            double imaginary2 = -Math.sqrt(-discriminant) / (2 * a);
 
-            System.out.println("3= " + imagin1);
-            System.out.println("3= " + imagin2);
-            System.out.println("3= " + real);
-
-
+            System.out.println("Real Part = " + real);
+            System.out.println("Imaginary Part 1 = " + imaginary1);
+            System.out.println("Imaginary Part 2 = " + imaginary2);
         }
-
-
     }
 }
